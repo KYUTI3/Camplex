6aeaa758da6f1865cf556e64bcf6fd9ff2ded90e
johnnyzheng111 <johnny.zheng@csedge.org>
2023-06-10 15:04:01 -0700

    changes

---
+++ README.md
@@ -0,0 +1,13 @@
+# Camplex
+Ever wondered what emotions someone is feeling? Camplex is an app that helps individuals feel better about themselves by boosting their self confidence.
+## Created by: Luis Cardenas and Johnny Zheng
+### What we used: 
+- Python Flask
+- RaspberryPi Camera
+- Google Cloud Vision API Services
+
+| Tool | Description |
+| ----------- | ----------- |
+| Python | Title |
+| Paragraph | Text |
+
--- app.py
+++ app.py
@@ -1,16 +1,19 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, Response
 import sqlite3
 import requests
 from picamera import PiCamera
-import io
+import os, io
 import cv2
 from google.cloud import vision
-
+# reminder for June 14th, 2023: put the emailed picture in README.md
 app = Flask(__name__)
 # path for the image to be tested
 path = "home/pi/Desktop/TEFinalProjectT3/images/"
 service_account_key_file = '/path/to/your/service_account_key.json'
 
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'GCVision.json' #the credetials to talk to the API.
+client = vision.ImageAnnotatorClient()
+image_path = "./images/image1.jpg"
 
 def validate_user(email, password):
     print("validating user...")
@@ -186,38 +189,87 @@ def post_user():
 #             'https://cloud.google.com/apis/design/errors'.format(
 #                 response.error.message))
 
-def detect_faces_uri(uri):
-    """Detects faces in the file located in Google Cloud Storage or the web."""
-    from google.cloud import vision
-    client = vision.ImageAnnotatorClient()
-    image = vision.Image()
-    image.source.image_uri = uri
+# def detect_faces_uri(uri):
+#     """Detects faces in the file located in Google Cloud Storage or the web."""
+#     from google.cloud import vision
+#     client = vision.ImageAnnotatorClient()
+#     image = vision.Image()
+#     image.source.image_uri = uri
 
-    response = client.face_detection(image=image)
-    faces = response.face_annotations
+#     response = client.face_detection(image=image)
+#     faces = response.face_annotations
 
-    # Names of likelihood from google.cloud.vision.enums
-    likelihood_name = ('UNKNOWN', 'VERY_UNLIKELY', 'UNLIKELY', 'POSSIBLE',
-                       'LIKELY', 'VERY_LIKELY')
-    print('Faces:')
+#     # Names of likelihood from google.cloud.vision.enums
+#     likelihood_name = ('UNKNOWN', 'VERY_UNLIKELY', 'UNLIKELY', 'POSSIBLE',
+#                        'LIKELY', 'VERY_LIKELY')
+#     print('Faces:')
 
-    for face in faces:
-        print(f'anger: {likelihood_name[face.anger_likelihood]}')
-        print(f'joy: {likelihood_name[face.joy_likelihood]}')
-        print(f'surprise: {likelihood_name[face.surprise_likelihood]}')
+#     for face in faces:
+#         print(f'anger: {likelihood_name[face.anger_likelihood]}')
+#         print(f'joy: {likelihood_name[face.joy_likelihood]}')
+#         print(f'surprise: {likelihood_name[face.surprise_likelihood]}')
 
-        vertices = ([f'({vertex.x},{vertex.y})'
-                    for vertex in face.bounding_poly.vertices])
+#         vertices = ([f'({vertex.x},{vertex.y})'
+#                     for vertex in face.bounding_poly.vertices])
 
-        print('face bounds: {}'.format(','.join(vertices)))
+#         print('face bounds: {}'.format(','.join(vertices)))
 
-    if response.error.message:
-        raise Exception(
-            '{}\nFor more info on error messages, check: '
-            'https://cloud.google.com/apis/design/errors'.format(
-                response.error.message))
+#     if response.error.message:
+#         raise Exception(
+#             '{}\nFor more info on error messages, check: '
+#             'https://cloud.google.com/apis/design/errors'.format(
+#                 response.error.message))
 
-print(detect_faces_uri("https://www.allprodad.com/wp-content/uploads/2021/03/05-12-21-happy-people.jpg"))
+# def get_image_from_frame(cap):
+#     ret, frame = cap.read()
+#     file = 'frame.png'
+#     cv2.imwrite(file,frame)
+#     cv2.imshow('frame',frame) #show camera output
+#     return file
+
+# def get_object_from_cloud(image_path):
+
+#     with io.open(image_path, 'rb') as image_file:
+#         content = image_file.read()
+
+#     image = vision.Image(content=content)
+#     response = client.label_detection(image=image) #full response from Google cloud vision
+#     label_annotations = response.label_annotations #array of objects labeled
+#     #print(label_annotations)
+#     description = label_annotations[0].description #get description of one object, the first being the one with the highest confidnce score.
+#     if description:
+#         return description
+#     else:
+#         return 'could not detect object'
+
+# def start_camera():
+#     global object_to_find
+#     os.system('sudo modprobe bcm2835-v4l2') #Force the Raspberry Pi to use the the Picamera, which CV2 will need to capture each frame.
+
+#     cap = cv2.VideoCapture(0)
+#     print("Starting camera")
+
+#     while True:
+        
+#         img = get_image_from_frame(cap)
+#         key = cv2.waitKey(0) #press 0 to move through frames
+#         object_to_find = get_object_from_cloud(img)
+
+#         print(object_to_find)
+
+#         if key == ord('q'): #press q to quit
+#             break
+    
+#     cap.release() #release the object when the app quits.
+#     cv2.destroyAllWindows()
+
+
+# start_camera()
+
+
+
+print("hi")
+# print(detect_faces_uri("https://www.allprodad.com/wp-content/uploads/2021/03/05-12-21-happy-people.jpg"))
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port='3000')
 
